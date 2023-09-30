0858152e0fc3fb664420931344864290218d3de7
This is XiaoDeng <1744793737@qq.com>
2023-09-30 17:03:00 +0800

    fix a bug

--- .github/rename.py
+++ .github/rename.py
@@ -3,4 +3,5 @@ import os
 if os.name == "nt":
     os.system('move build\\main.exe build\\onedisc.exe')
 else:
-    os.system('mv build/main.exe build/onedisc.exe')
+    os.system('mv build/main.bin build/onedisc')
+
