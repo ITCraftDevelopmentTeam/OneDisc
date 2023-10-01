70114eb8e74f275ac7cbd82b567a46def7a3721b
This is XiaoDeng <1744793737@qq.com>
2023-10-01 18:48:15 +0800

    update build-dev.yml

--- .github/rename.py
+++
@@ -1,7 +0,0 @@
-import os
-
-if os.name == "nt":
-    os.system('move build\\main.exe build\\onedisc.exe')
-else:
-    os.system('mv build/main.bin build/onedisc')
-
--- version.py
+++ version.py
@@ -1,4 +1,3 @@
 VERSION = "0.1.0"
 
-if __name__ == "__main__":
-    print(f"VERSION={VERSION}.0")
+
