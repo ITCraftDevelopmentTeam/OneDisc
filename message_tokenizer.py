c8fd46283e3ff17e442544ffc5155a666ddf6c86
This is XiaoDeng <1744793737@qq.com>
2023-10-02 17:47:05 +0800

    修改分词器

--- message_tokenizer.py
+++ message_tokenizer.py
@@ -1,29 +1,24 @@
 import re
 
-def tokenizer(code):
-    # 定义正则表达式模式
-    token_patterns = {
-        'mention': '<@[0-9]+>',
-        'mention_all': '@everyone',
-        'dc.emoji': '<:.+:[0-9]+>',
-        'text': '.+'
-    }
-    
-    tokens = []
-    code_remaining = code
+token_patterns = [
+    ('mention', '<@[0-9]+>'),
+    ('mention_all', '@everyone'),
+    ('emoji', '<:.+?:[0-9]+>'),
+    ('text', '<(?![@:]).*?(?=<|$)|[^<@:]+')  # 使用正向否定断言来匹配不后跟@或:的<
+]
 
-    while code_remaining:
-        matched = False
-        for token_type, pattern in token_patterns.items():
-            match = re.match(pattern, code_remaining)
-            if match:
-                token_value = match.group(0)
-                tokens.append((token_type, token_value))
-                code_remaining = code_remaining[len(token_value):]
-                matched = True
+def tokenizer(code):
+    tokens = []
+    combined_pattern = '|'.join('(?P<%s>%s)' % (name, pattern) for name, pattern in token_patterns)
+    for match in re.finditer(combined_pattern, code):
+        for name, pattern in token_patterns:
+            if match.group(name):
+                tokens.append((name, match.group(name)))
                 break
-        
-        if not matched:
-            raise ValueError(f"Unexpected character: '{code_remaining[0]}'")
-    
+
     return tokens
+
+if __name__ == "__main__":
+    s = '<:avatar_xdbot:1133052388438188143><XDbot> <@1006777034564968498>  冷却中（1588.332s）'
+    print(tokenizer(s))
+
