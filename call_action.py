b9dea0451ecf94753485f5f11d97c3beedce151f
This is XiaoDeng <1744793737@qq.com>
2023-10-03 23:56:45 +0800

    添加更多异常处理

--- call_action.py
+++ call_action.py
@@ -17,6 +17,8 @@ def init(conf: dict) -> None:
 async def on_call_action(action: str, params: dict, echo: str | None = None, **_) -> dict:
     if config['system'].get("allow_strike") and random.random() <= 0.1:
         return return_object.get(36000)
+    if action not in action_list.keys():
+        return return_object.get(10002, "action not found")
     try:
         return_data = await action_list[action](**params)
     except UnsupportedSegment as e:
@@ -25,6 +27,12 @@ async def on_call_action(action: str, params: dict, echo: str | None = None, **_
         return return_object.get(10006, str(e))
     except BadParam as e:
         return return_object.get(10003, str(e))
+    except TypeError as e:
+        if "got an unexpected keyword argument" in str(e):
+            return return_object.get(10004, str(e))
+        else:
+            logger.error(traceback.format_exc())
+            return_data = return_object.get(20002, str(e))
     except Exception as e:
         logger.error(traceback.format_exc())
         return_data = return_object.get(20002, str(e))
