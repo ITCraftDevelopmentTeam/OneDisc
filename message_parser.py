7a0efa28b38032237339459579561efa1572b0e5
This is XiaoDeng <1744793737@qq.com>
2023-10-04 15:38:11 +0800

    格式化代码

--- message_parser.py
+++ message_parser.py
@@ -6,21 +6,30 @@ import message_tokenizer
 import re
 import discord.file
 
-class BadSegmentData(Exception): pass
-class UnsupportedSegment(Exception): pass
+
+class BadSegmentData(Exception):
+    pass
+
+
+class UnsupportedSegment(Exception):
+    pass
+
 
 client: discord.Client
 logger = get_logger()
+
 
 def init(_client: discord.Client):
     global client
     client = _client
 
+
 def escape_mentions(text):
-    text = text.replace('<', '<\u200B').replace('>', '\u200B>')
-    text = text.replace('@everyone', '@\u200Beveryone')
-    text = text.replace('@here', '@\u200Bhere')
+    text = text.replace("<", "<\u200B").replace(">", "\u200B>")
+    text = text.replace("@everyone", "@\u200Beveryone")
+    text = text.replace("@here", "@\u200Bhere")
     return text
+
 
 def parse_message(message: list) -> dict:
     logger.debug(config)
@@ -35,11 +44,22 @@ def parse_message(message: list) -> dict:
                 case "mention_all":
                     message_data["content"] += "@everyone"
                 case "image" | "voice" | "audio" | "video" | "file":
-                    message_data["files"].append(discord.file.File(open(file.get_file_path(file.get_file_name_by_id(segment["data"]["file_id"])), "rb")))
+                    message_data["files"].append(
+                        discord.file.File(
+                            open(
+                                file.get_file_path(
+                                    file.get_file_name_by_id(segment["data"]["file_id"])
+                                ),
+                                "rb",
+                            )
+                        )
+                    )
                     if segment["type"] == "voice":
                         logger.warning("OneDisc 暂不支持 voice 消息段，将以 audio 消息段发送")
                 case "dc.emoji":
-                    message_data["content"] += f'<:{segment["data"]["name"]}:{segment["data"]["id"]}>'
+                    message_data[
+                        "content"
+                    ] += f'<:{segment["data"]["name"]}:{segment["data"]["id"]}>'
                 case "reply":
                     for msg in client.cached_messages:
                         if msg.id == int(segment["data"]["message_id"]):
@@ -47,7 +67,7 @@ def parse_message(message: list) -> dict:
                             break
                     else:
                         logger.warning(f"解析消息段 {segment} 时出现错误：找不到指定消息，已忽略")
-                
+
                 case _:
                     if config["system"].get("ignore_unsupported_segment"):
                         logger.warning(f"不支持的消息段类型：{segment['type']}，已忽略")
@@ -60,38 +80,27 @@ def parse_message(message: list) -> dict:
     logger.debug(message_data)
     return message_data
 
+
 def parse_string(string: str) -> list:
     message = []
     tokenized_messages = message_tokenizer.tokenizer(string)
     for token in tokenized_messages:
         match token[0]:
-            case 'mention':
-                message.append({
-                    "type": "mention",
-                    "data": {
-                        "user_id": token[1][2:-1]
-                    }
-                })
-            case 'mention_all':
-                message.append({
-                    "type": 'mention_all',
-                    'data': {}
-                })
+            case "mention":
+                message.append({"type": "mention", "data": {"user_id": token[1][2:-1]}})
+            case "mention_all":
+                message.append({"type": "mention_all", "data": {}})
             case "text":
-                message.append({
-                    "type": "text",
-                    "data": {
-                        "text": token[1]
-                    }
-                })
+                message.append({"type": "text", "data": {"text": token[1]}})
             case "emoji":
-                message.append({
-                    "type": "discord.emoji",
-                    "data": {
-                        "name": re.search(":.+:", token[1]).group(0)[1:-1],   # type: ignore
-                        "id": int(re.search("[0-9]+>", token[1]).group(0)[:-1])  # type: ignore
+                message.append(
+                    {
+                        "type": "discord.emoji",
+                        "data": {
+                            "name": re.search(":.+:", token[1]).group(0)[1:-1],  # type: ignore
+                            "id": int(re.search("[0-9]+>", token[1]).group(0)[:-1]),  # type: ignore
+                        },
                     }
-                })
+                )
     logger.debug(message)
     return message
-    
