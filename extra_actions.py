f0da33044a6aba31526219bb44461e493ff1e7be
This is XiaoDeng <1744793737@qq.com>
2023-10-21 18:02:24 +0800

    添加 edit_message 动作

---
+++ extra_actions.py
@@ -0,0 +1,25 @@
+from api import register_action
+from client import client
+import return_object
+import message_parser
+import message_parser_v11
+import translator
+
+
+@register_action()
+async def edit_message(message_id: str, content: list) -> dict:
+    for message in client.cached_messages:
+        if message.id == int(message_id):
+            await message.edit(content=message_parser.parse_message(content)["content"])
+            return return_object.get(0)
+    return return_object.get(35002, f"消息 {message_id} 不存在")
+
+@register_action("v11")
+async def edit_message(message_id: int, content: list | str) -> dict:
+    if isinstance(content, str):
+        message = message_parser_v11.parse_text(content)
+    else:
+        message = content.copy()
+    message = translator.translate_message_array(message)
+    return await edit_message(str(message_id), message)
+
