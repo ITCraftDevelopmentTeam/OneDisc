009b81160c08c23ea393bbf912503cfb6b47315e
This is XiaoDeng <1744793737@qq.com>
2023-11-25 18:11:17 +0800

    添加动作：call_api(endpoint: str, method: str, data: dict)

--- extra_actions.py
+++ extra_actions.py
@@ -1,10 +1,12 @@
 from api import register_action
+import httpx
 from client import client
+from logger import discord_api_failed
 import return_object
 import message_parser
+from config import config
 import message_parser_v11
 import translator
-
 
 @register_action()
 async def edit_message(message_id: str, content: list) -> dict:
@@ -23,3 +25,16 @@ async def edit_message_v11(message_id: int, content: list | str) -> dict:
     message = await translator.translate_message_array(message)
     return await edit_message(str(message_id), message)
 
+@register_action()
+async def call_api(endpoint: str, method: str, data: dict) -> dict:
+    async with httpx.AsyncClient(proxies=config["system"]["proxy"]) as client:
+        response = await client.request(
+            method,
+            f"https://discord.com/api/v9/{endpoint}",
+            json=data
+        )
+    return return_object.get(
+        0,
+        status_code=response.status_code,
+        response=response.json()
+    )
