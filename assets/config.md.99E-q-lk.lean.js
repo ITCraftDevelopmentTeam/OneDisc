b825895f059746cbbd39be8c814528556fc688c0
This-is-XiaoDeng <This-is-XiaoDeng@users.noreply.github.com>
2023-12-31 06:34:34 +0000

    deploy: b56df9c0dc15ef0728c16455f637a13dccc3aaa7

---
+++ assets/config.md.99E-q-lk.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,U as a}from"./chunks/framework.qwUbILKO.js";const g=JSON.parse('{"title":"配置","description":"","frontmatter":{},"headers":[],"relativePath":"config.md","filePath":"config.md"}'),i={name:"config.md"},d=a("",108),n=[d];function l(h,o,r,c,p,k){return s(),e("div",null,n)}const E=t(i,[["render",l]]);export{g as __pageData,E as default};
---
+++ assets/quickstart.md.fBIkANG0.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,U as e}from"./chunks/framework.qwUbILKO.js";const g=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"quickstart.md","filePath":"quickstart.md"}'),t={name:"quickstart.md"},n=e("",20),l=[n];function h(p,o,k,c,d,r){return a(),i("div",null,l)}const u=s(t,[["render",h]]);export{g as __pageData,u as default};
