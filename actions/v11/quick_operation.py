ea2e09d9c2155a78aebb7e663485e895c467e93f
This is XiaoDeng <1744793737@qq.com>
2024-01-07 11:52:20 +0800

    实现 `.handle_quick_operation` 动作

--- actions/v11/quick_operation.py
+++ actions/v11/quick_operation.py
@@ -1,6 +1,8 @@
 import traceback
 from typing import Awaitable, Callable
 from utils.logger import get_logger
+from utils import return_object
+from actions import register_action
 
 handlers: dict[str, dict[str, Callable[..., Awaitable[None]]]] = {}
 logger = get_logger()
@@ -30,5 +32,11 @@ def register_quick_operation(post_type: str, detail_type: str) -> Callable[..., 
         if post_type not in handlers.keys():
             handlers[post_type] = {}
         handlers[post_type][detail_type] = func
+        logger.debug(f"成功注册快速操作: {post_type}.{detail_type}")
         return func
     return _
+
+@register_action("v11", ".handle_quick_operation")
+async def _(context: dict, operation: dict) -> dict:
+    await handle_quick_operation(operation, context)
+    return return_object.get()
