cdfad7ec3e4834546a1a2603d106720c51e0088f
This-is-XiaoDeng <This-is-XiaoDeng@users.noreply.github.com>
2024-01-17 14:01:45 +0000

    deploy: a1c50efed42dac1f0e3939d306362b641983efba

---
+++ assets/config.md.tyjfw_rq.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,V as a}from"./chunks/framework.l0MLLx4M.js";const g=JSON.parse('{"title":"配置","description":"","frontmatter":{},"headers":[],"relativePath":"config.md","filePath":"config.md"}'),i={name:"config.md"},d=a("",115),n=[d];function l(h,o,r,c,p,k){return s(),e("div",null,n)}const E=t(i,[["render",l]]);export{g as __pageData,E as default};
