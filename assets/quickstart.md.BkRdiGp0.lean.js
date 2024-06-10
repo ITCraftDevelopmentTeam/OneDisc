3fe7c01009e64a8d9cba14834ed72d662bb7f436
This-is-XiaoDeng <This-is-XiaoDeng@users.noreply.github.com>
2024-06-10 12:21:01 +0000

    deploy: 403fc016a387c8b8b0b49eb7b3eeaafc8519e1cd

---
+++ assets/quickstart.md.BkRdiGp0.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as i,a1 as e}from"./chunks/framework.DCKU21so.js";const g=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"quickstart.md","filePath":"quickstart.md"}'),t={name:"quickstart.md"},n=e("",20),l=[n];function h(p,o,k,c,d,r){return i(),a("div",null,l)}const u=s(t,[["render",h]]);export{g as __pageData,u as default};
