a1a84d93df03faf37972a78f0610b44cc88ea3a7
This-is-XiaoDeng <This-is-XiaoDeng@users.noreply.github.com>
2024-06-23 06:33:52 +0000

    deploy: 1b9463d99c316d0e8f6fa26f979cfb7d98b20629

--- assets/config.md.B2xFAMt1.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a,a1 as s}from"./chunks/framework.DCKU21so.js";const g=JSON.parse('{"title":"配置","description":"","frontmatter":{},"headers":[],"relativePath":"config.md","filePath":"config.md"}'),i={name:"config.md"},d=s("",132),n=[d];function l(h,o,r,c,p,k){return a(),e("div",null,n)}const u=t(i,[["render",l]]);export{g as __pageData,u as default};
